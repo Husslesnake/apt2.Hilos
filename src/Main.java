a4e76ea736ae321e9c47766fb40363c276889439
Husslesnake <adrianbravosantos@gmail.com>
2025-11-21 13:12:04 +0100

    Añadido 4 balsas y Barco, y el metodo rescate.run

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
+        Barco alianza = new Barco(352);
 
-        //Completar Codigo
+        List<Balsa> balsas = Arrays.asList(
+                new Balsa("Acasta", 1, 0.5),
+                new Balsa("Banff", 2, 1),
+                new Balsa("Cadiz", 3, 2),
+                new Balsa("Deimos", 4, 4),
+                new Balsa("Expedición", 5, 8)
+        );
 
+        Rescate rescate = new Rescate(balsas, alianza.getPasajeros());
+        rescate.run();
     }
 }

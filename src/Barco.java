a77333b2b3895225b6f829152344cff7e3b724a9
Husslesnake <adrianbravosantos@gmail.com>
2025-12-11 20:26:41 +0100

    Commit

--- src/Barco.java
+++ src/Barco.java
@@ -1,15 +1,20 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Barco {
-    private List<Pasajero> pasajeros = new ArrayList<>();
 
-    public Barco(int totalPasajeros) {
-        Random random = new Random();
-        for (int i = 1; i <= totalPasajeros; i++) {
-            int prioridad = 1 + random.nextInt(4); // prioridad 1–4
-            pasajeros.add(new Pasajero(i, prioridad));
-        }
+    private final List<Pasajero> pasajeros = new ArrayList<>();
+
+    public Barco(List<Pasajero> pasajeros) {
+        this.pasajeros.addAll(pasajeros);
     }
 
-    public List<Pasajero> getPasajeros() { return pasajeros; }
+    public List<Pasajero> getPasajeros() {
+        return pasajeros;
+    }
+
+    public boolean hayPasajeros(){
+        return !pasajeros.isEmpty();
+    }
+
 }
